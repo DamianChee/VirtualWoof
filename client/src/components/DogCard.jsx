f43b6f83cb14034b234a1dfa4711fad0244de5cb
thisisanita <anita.muthukumarasamy@gmail.com>
2024-04-05 22:59:32 +0800

    wip

--- client/src/components/DogCard.jsx
+++ client/src/components/DogCard.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import DogList from "./DogList";
 import Button from "./Button";
+import useFetch from "../hooks/useFetch";
+import userContext from "../context/user";
 
 const DogCard = (props) => {
+  const userCtx = useContext(userContext);
+  const fetchData = useFetch();
   console.log(props.selectedDog);
+  console.log(props.selectedDog.breed);
+
+  const addDog = async () => {
+    const res = await fetchData(
+      "/api/dogs",
+      "PUT",
+      {
+        breed: props.selectedDog.breed,
+        size: props.selectedDog.size,
+        personality: props.selectedDog.personality,
+        coat: props.selectedDog.coat,
+      },
+      userCtx.accessToken
+    );
+
+    if (res.ok) {
+      console.log("sucessfully addded dog");
+    } else {
+      alert(JSON.stringify(res.data));
+      console.log(res.data);
+    }
+  };
+
+  if (props.selectedDog) {
+    addDog();
+  }
   return (
     <>
       <div>{props.selectedDog.breed}</div>
--- client/src/components/SelectDog.jsx
+++ client/src/components/SelectDog.jsx
@@ -7,34 +7,7 @@ import DogList from "./DogList";
 
 const SelectDog = (props) => {
   const dogs = props.dogs;
-  // const dogs = [
-  //   {
-  //     breed: "Poodle",
-  //     personality: "Friendly and sensitive",
-  //     coat: "curly",
-  //     size: "medium",
-  //     imageUrl: Dog1,
-  //   },
-  //   {
-  //     breed: "Beagle",
-  //     personality: "Friendly and loyal",
-  //     coat: "straight",
-  //     size: "medium",
-  //     imageUrl: Dog2,
-  //   },
-  //   {
-  //     breed: "Chihuahua",
-  //     personality: "Friendly and playful",
-  //     coat: "straight",
-  //     size: "medium",
-  //     imageUrl: Dog3,
-  //   },
-  // ];
-
-  // const handleSelectedDog = (dog) => {
-  //   console.log("Selected dog:", dog);
-  //   setSelectedDog(dog);
-  // };
+  console.log(props.selectedDog);
 
   return (
     <div>
