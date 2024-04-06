009f8972d81bf2f491ab24bff0f72d2c47ab542d
thisisanita <anita.muthukumarasamy@gmail.com>
2024-04-06 15:30:03 +0800

    wip

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Task from "./pages/Task";
 import NotFound from "./pages/NotFound";
+import UserContext from "./context/user";
 
 /* 
 
@@ -13,16 +14,20 @@ main is the dog stuff
 */
 
 function App() {
+  const [accessToken, setAccessToken] = useState("");
+  const [showLogin, setShowLogin] = useState(true);
   return (
     <div>
-      <Routes>
-        {/* <Route path="/" element={<Navigate replace to="/login" />}></Route> */}
-        <Route path="main" element={<Main />}></Route>
-        <Route path="login" element={<Login />}></Route>
-        <Route path="register" element={<Register />}></Route>
-        <Route path="task" element={<Task />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
+      <UserContext.Provider value={{ accessToken, setAccessToken }}>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/login" />}></Route>
+          <Route path="main" element={<Main />}></Route>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="register" element={<Register />}></Route>
+          <Route path="task" element={<Task />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </UserContext.Provider>
     </div>
   );
 }
--- client/src/components/DogCard.jsx
+++ client/src/components/DogCard.jsx
@@ -2,14 +2,23 @@ import React, { useContext, useEffect, useRef, useState } from "react";
 import DogList from "./DogList";
 import Button from "./Button";
 import useFetch from "../hooks/useFetch";
-import userContext from "../context/user";
+import UserContext from "../context/user";
 import GoalList from "./GoalList";
 
 const DogCard = (props) => {
-  const userCtx = useContext(userContext);
+  const userCtx = useContext(UserContext);
   const fetchData = useFetch();
+  const [dogbyOwner, setDogByOwner] = useState([]);
   console.log(props.selectedDog);
   console.log(props.selectedDog.breed);
+  console.log(userCtx);
+
+  useEffect(() => {
+    if (props.selectedDog) {
+      addDog();
+      getDogbyOwner();
+    }
+  }, [props.selectedDog]);
 
   const addDog = async () => {
     const res = await fetchData(
@@ -32,15 +41,33 @@ const DogCard = (props) => {
     }
   };
 
-  if (props.selectedDog) {
-    addDog();
-  }
+  const getDogbyOwner = async () => {
+    const res = await fetchData(
+      "/api/dogs/id",
+      "POST",
+      {
+        owner: "660e1c58b23ff2bdba967db5",
+      }, // need to change this to dynamically reflect the userid
+      userCtx.accessToken
+    );
+    if (res.ok) {
+      setDogByOwner(res.data);
+    } else {
+      alert(JSON.stringify(res.data));
+      console.log(res.data);
+    }
+  };
+
   return (
     <>
-      <div>{props.selectedDog.breed}</div>
+      {/* <div>{props.selectedDog?.breed}</div>
+      {dogbyOwner.map((dog, idx) => (
+        <div key={idx}>{dog.breed}</div>
+      ))}
+      <div>{dogbyOwner.breed}</div>
       <Button>Feed</Button>
       <Button>Train</Button>
-      <Button>Play</Button>
+      <Button>Play</Button> */}
     </>
   );
 };
