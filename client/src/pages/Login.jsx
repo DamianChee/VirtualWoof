5b48aff7dc5048c4e47f40723b9af97b7028001b
Xenousss <ttng001@gmail.com>
2024-04-08 09:52:58 +0800

    update login and register

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,20 +3,12 @@ import UserContext from "../context/user";
 import LoginUser from "../components/Login";
 import styles from "../components/PageStyles.module.css";
 
-/* 
-
-currently, login is not working
-
- */
-
 const Login = () => {
   const [accessToken, setAccessToken] = useState("");
 
   return (
     <div className={`${styles.backgroundpage}`}>
-      {/* <UserContext.Provider value={{ accessToken, setAccessToken }}> */}
       <LoginUser></LoginUser>
-      {/* </UserContext.Provider> */}
     </div>
   );
 };
