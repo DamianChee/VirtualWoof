a9c1a1f0aac6c260831b3d597c8cfaa9e4124342
Damian Chee <silverousmechs@gmail.com>
2024-04-08 10:51:04 +0800

    Create & Add endpoint protection

--- server/src/routers/dogs.js
+++ server/src/routers/dogs.js
@@ -8,14 +8,15 @@ const {
   getDogsByOwner,
   getDogById,
 } = require("../controllers/dogs");
+const { authUser } = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/dogs/seed", seedDogs);
-router.get("/dogs", getAllDogs);
-router.post("/dogs/id", getDogById);
-router.post("/dogs/owner", getDogsByOwner);
-router.put("/dogs", addDog);
-router.patch("/dogs", updateDog);
-router.delete("/dogs", deleteDog);
+router.get("/dogs", authUser, getAllDogs);
+router.post("/dogs/id", authUser, getDogById);
+router.post("/dogs/owner", authUser, getDogsByOwner);
+router.put("/dogs", authUser, addDog);
+router.patch("/dogs", authUser, updateDog);
+router.delete("/dogs", authUser, deleteDog);
 
 module.exports = router;
--- server/src/routers/users.js
+++ server/src/routers/users.js
@@ -11,17 +11,18 @@ const {
   refresh,
   getUserById,
 } = require("../controllers/users");
+const { authUser } = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/users/seed", seedUsers);
 router.get("/users", getAllUsers);
 router.put("/users", addUser);
 router.put("/users/adopt", giveUserDog);
-router.patch("/users", updateUser);
-router.delete("/users", deleteUser);
+router.patch("/users", authUser, updateUser);
+router.delete("/users", authUser, deleteUser);
 router.post("/users/register", register);
 router.post("/users/login", login);
-router.post("/users/refresh", refresh);
-router.post("/users/userid", getUserById);
+router.post("/users/refresh", authUser, refresh);
+router.post("/users/userid", authUser, getUserById);
 
 module.exports = router;
