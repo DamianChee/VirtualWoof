734a198961f24c6db5433a0fca6675c1faa1f0ee
Xenousss <ttng001@gmail.com>
2024-04-08 18:50:15 +0800

    Update Main.jsx

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -24,7 +24,7 @@ const Main = () => {
   const [userById, setUserById] = useState({});
   const [tasks, setTasks] = useState([]);
 
-  console.log(dogByOwner[0]);
+  console.log(userCtx.userById);
   // const dogId = dogByOwner[0]._id;
   // console.log(dogId);
 
